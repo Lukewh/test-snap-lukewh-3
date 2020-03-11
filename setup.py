770d9d64e57963d21474014c76508f74a385e34e
Lukewh <luke@lukewh.com>
2020-03-11 08:57:04 +0000

    Update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="test-snap-lukewh-3",
-    version="0.0.1",
+    version="0.0.6",
     author="Luke Wesley-Holley",
     author_email="luke.wesley-holley@canonical.com",
     py_modules=[],
